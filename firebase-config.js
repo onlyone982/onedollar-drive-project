61e729a5d3c17fa711ea454aab3bb548814fdaa7
nerd <sicknerd76@gmail.com>
2025-11-05 23:36:43 +0900

    3th try chat closed

--- firebase-config.js
+++ firebase-config.js
@@ -20,3 +20,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export { app };
